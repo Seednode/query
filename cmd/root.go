dea03d650f36bd3cc8f63176b404ff9c5d6257e7
Seednode <git@seedno.de>
2023-08-29 09:12:54 -0500

    Added query parameter formatting for time strings

--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	Version string = "0.2.1"
+	Version string = "0.3.0"
 )
 
 var (
--- cmd/time.go
+++ cmd/time.go
@@ -13,9 +13,28 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func serveTime() httprouter.Handle {
+func serveTime(TimeFormats map[string]string) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
 		startTime := time.Now()
+
+		var format string = ""
+
+		requestedFormat := r.URL.Query().Get("format")
+
+		if requestedFormat != "" {
+			for k, v := range TimeFormats {
+				if strings.EqualFold(requestedFormat, k) {
+					format = v
+
+					break
+				}
+			}
+		}
+
+		if format == "" {
+			format = TimeFormats["RFC822"]
+		}
 
 		tz, err := time.LoadLocation(strings.TrimPrefix(p[0].Value, "/"))
 
@@ -27,7 +46,7 @@ func serveTime() httprouter.Handle {
 
 		w.Header().Set("Content-Type", "text/plain")
 
-		w.Write([]byte(startTime.String() + "\n"))
+		w.Write([]byte(startTime.Format(format) + "\n"))
 
 		if verbose {
 			fmt.Printf("%s | %s checked the time\n",
