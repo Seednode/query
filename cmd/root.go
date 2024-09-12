744b1d9c1fa23783ef77102aa97a5e5638da6901
Seednode <git@seedno.de>
2024-09-12 09:27:23 -0500

    Remove redundant wording in usage output

--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	ReleaseVersion string = "1.18.3"
+	ReleaseVersion string = "1.18.4"
 )
 
 var (
@@ -80,25 +80,25 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().BoolVar(&all, "all", false, "enable all functionality")
+	rootCmd.Flags().BoolVar(&all, "all", false, "enable all features")
 	rootCmd.Flags().StringVarP(&bind, "bind", "b", "0.0.0.0", "address to bind to")
-	rootCmd.Flags().BoolVar(&dns, "dns", false, "enable DNS lookup functionality")
+	rootCmd.Flags().BoolVar(&dns, "dns", false, "enable DNS lookup")
 	rootCmd.Flags().StringVar(&dnsResolver, "dns-resolver", "", "custom DNS server IP and port to query (e.g. 8.8.8.8:53)")
 	rootCmd.Flags().BoolVar(&exitOnError, "exit-on-error", false, "shut down webserver on error, instead of just printing the error")
-	rootCmd.Flags().BoolVar(&hashing, "hash", false, "enable hashing functionality")
-	rootCmd.Flags().BoolVar(&httpStatus, "http-status", false, "enable HTTP response status code functionality")
-	rootCmd.Flags().BoolVar(&ip, "ip", false, "enable IP lookup functionality")
-	rootCmd.Flags().BoolVar(&mac, "mac", false, "enable MAC lookup functionality")
+	rootCmd.Flags().BoolVar(&hashing, "hash", false, "enable hashing")
+	rootCmd.Flags().BoolVar(&httpStatus, "http-status", false, "enable HTTP response status codes")
+	rootCmd.Flags().BoolVar(&ip, "ip", false, "enable IP lookups")
+	rootCmd.Flags().BoolVar(&mac, "mac", false, "enable MAC lookups")
 	rootCmd.Flags().IntVar(&maxDiceRolls, "max-dice-rolls", 1024, "maximum number of dice per roll")
 	rootCmd.Flags().IntVar(&maxDiceSides, "max-dice-sides", 1024, "maximum number of sides per die")
 	rootCmd.Flags().StringVar(&ouiFile, "oui-file", "", "path to Wireshark manufacturer database file")
 	rootCmd.Flags().Uint16VarP(&port, "port", "p", 8080, "port to listen on")
 	rootCmd.Flags().BoolVar(&profile, "profile", false, "register net/http/pprof handlers")
-	rootCmd.Flags().BoolVar(&qr, "qr", false, "enable QR code generation functionality")
+	rootCmd.Flags().BoolVar(&qr, "qr", false, "enable QR code generation")
 	rootCmd.Flags().IntVar(&qrSize, "qr-size", 256, "height/width of PNG-encoded QR codes (in pixels)")
-	rootCmd.Flags().BoolVar(&roll, "roll", false, "enable dice rolling functionality")
-	rootCmd.Flags().BoolVar(&subnet, "subnet", false, "enable subnet calculator functionality")
-	rootCmd.Flags().BoolVar(&timezones, "time", false, "enable time lookup functionality")
+	rootCmd.Flags().BoolVar(&roll, "roll", false, "enable dice rolls")
+	rootCmd.Flags().BoolVar(&subnet, "subnet", false, "enable subnet calculator")
+	rootCmd.Flags().BoolVar(&timezones, "time", false, "enable time lookup")
 	rootCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "log tool usage to stdout")
 	rootCmd.Flags().BoolVarP(&version, "version", "V", false, "display version and exit")
 
